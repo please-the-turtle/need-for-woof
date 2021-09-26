648c08fc4a35d394495eed6a09128be5dbd248fd
Alexey Viklov <alexey.viklov@gmail.com>
2021-09-27 00:07:03 +0300

    sound and music sliders was added

--- NeedForWoof/Scripts/SettingsScreen/PortLine.cs
+++ NeedForWoof/Scripts/SettingsScreen/PortLine.cs
@@ -3,22 +3,22 @@ using Godot;
 
 namespace NeedForWoof.Scripts.SettingsScreen
 {
-    public class PortLine : LineEdit
-    {
-        public override void _Ready()
-        {
-            base._Ready();
+	public class PortLine : LineEdit
+	{
+		public override void _Ready()
+		{
+			base._Ready();
 
-            Text = Global.NetworkPort.ToString();
-        }
+			Text = Global.NetworkPort.ToString();
+		}
 
-        public void OnPortLine_text_changed(string newText)
-        {
-            int newPort;
-            if (Int32.TryParse(newText, out newPort))
-            {
-                Global.NetworkPort = newPort;
-            }
-        }
-    }
+		public void OnPortLine_text_changed(string newText)
+		{
+			int newPort;
+			if (Int32.TryParse(newText, out newPort))
+			{
+				Global.NetworkPort = newPort;
+			}
+		}
+	}
 }
