c5f1a1527617a4421db9617a16d19eabd765e177
Alexey Viklov <alexey.viklov@gmail.com>
2021-10-09 14:10:08 +0300

    Ip label on the lobby screen

--- NeedForWoof/Scripts/LobbyScreen/IpLabel.cs
+++ NeedForWoof/Scripts/LobbyScreen/IpLabel.cs
@@ -6,8 +6,23 @@ namespace NeedForWoof.Scripts.LobbyScreen
     {
         public override void _Ready()
         {
-            var ip = IP.GetLocalAddresses();
-            Text = ip.ToString();
+            Text = GetLocalIpString();
+        }
+        
+        private string GetLocalIpString()
+        {
+            string ip = IP.GetLocalAddresses().ToString();
+
+            RegEx regex = new RegEx();
+            regex.Compile(@"192.168.\d+.\d+");
+            
+            var regexSearchResult = regex.Search(ip);
+            if (regexSearchResult is null)
+            {
+                return "Local network not found.";
+            }
+
+            return regexSearchResult.GetString();
         }
     }
 }
