b05c3bf81af86c68cfae5609f0997b8fc51e2312
Alexey Viklov <alexey.viklov@gmail.com>
2022-01-22 01:37:37 +0300

    Button positioning

---
+++ NeedForWoof/Scripts/OnScreenPosition.cs
@@ -0,0 +1,23 @@
+namespace NeedForWoof
+{
+    // TODO Unused
+
+    /// <summary>
+    /// Enumeration the positions of elements on the screen.<br/>
+    /// Vertically: Top, Middle, Bottom;<br/>
+    /// Horizontally: Left, Center, Right.
+    /// </summary>
+    public enum OnScreenPosition
+    {
+        Center,
+        BottomLeft,
+        BottomCenter,
+        BottomRight,
+        MiddleLeft,
+        MiddleCenter,
+        MiddleRight,
+        TopLeft,
+        TopCenter,
+        TopRight
+    }
+}
