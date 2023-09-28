f3c516bf70d5e1d301889a8872ce63ab0ade503d
Alexey Viklov <alexey@pop-os.localdomain>
2023-09-28 20:20:31 +0300

    Main menu to level scene transition added

--- game/scripts/ui/level_gui.gd
+++ game/scripts/ui/level_gui.gd
@@ -9,3 +9,8 @@ extends CanvasLayer
 
 func _process(_delta):
 	stamina_bar.value = target.stamina / target.max_stamina * 100
+
+
+func _on_home_button_pressed():
+	var scene_changer = get_node("/root/SceneChanger")
+	scene_changer.go_to_scene("res://scenes/ui/main_menu.tscn")
