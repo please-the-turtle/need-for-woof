d28f484a341e56b4febefbdcc92df27b82919e93
Alexey Viklov <alexey@pop-os.localdomain>
2023-10-19 10:48:11 +0300

    Message dialog created

---
+++ game/scripts/ui/message_dialog.gd
@@ -0,0 +1,39 @@
+extends PanelContainer
+
+
+signal ok_pressed
+
+
+var header: String = "Ooops!":
+	get:
+		return $VBoxContainer/Header.text
+	set(value):
+		$VBoxContainer/Header.text = value
+
+
+var message: String = "Something wrong":
+	get:
+		return $VBoxContainer/Message.text
+	set(value):
+		$VBoxContainer/Message.text = value
+
+
+@onready var _animation: AnimationPlayer = $AnimationPlayer
+
+
+func _ready():
+	visible = false
+	open()
+
+
+func open():
+	_animation.play("open")
+
+
+func close():
+	_animation.play("close")
+
+
+func _on_ok_button_pressed():
+	close()
+	emit_signal("ok_pressed")
