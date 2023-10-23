6dc6bd9c0a010a692dd1999e2b12df0f4e68eda8
Alexey Viklov <alexey.viklov@gmail.com>
2023-10-23 23:32:56 +0300

    Message dialog updated

--- game/scripts/ui/message_dialog.gd
+++ game/scripts/ui/message_dialog.gd
@@ -18,6 +18,19 @@ var message: String = "Something wrong":
 		$VBoxContainer/Message.text = value
 
 
+var back_on_ok: bool = false
+
+
+## Used for position animation
+@export var position_offset: Vector2 = Vector2.ZERO:
+	get: 
+		return position_offset
+	set(value):
+		position -= position_offset
+		position_offset = value
+		position += position_offset
+
+
 @onready var _animation: AnimationPlayer = $AnimationPlayer
 
 
@@ -36,3 +49,5 @@ func close():
 func _on_ok_button_pressed():
 	close()
 	emit_signal("ok_pressed")
+	if back_on_ok:
+		SceneChanger.go_to_previous_scene()
