a57ffb9ba62c144a293cbe36562627a30c4d25aa
Alexey Viklov <alexey.viklov@gmail.com>
2023-12-14 22:25:17 +0300

    Test scene updated

--- game/scripts/level/test_scene.gd
+++ game/scripts/level/test_scene.gd
@@ -2,4 +2,13 @@ extends Node2D
 
 
 func _ready():
-	pass
+	$FinishLine.dog_finished.connect(_put_dog_on_start)
+	%Countdown.is_over.connect(func():
+		$Siba.fsm.transition_to("DogStateRun")
+	)
+	%Countdown.start()
+
+
+func _put_dog_on_start(dog: Dog):
+	#dog.position = Vector2(241, 413)
+	dog.fsm.transition_to("DogStateIdle")
