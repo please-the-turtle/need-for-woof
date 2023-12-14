8aa33cfd90d34a118501552194222bbee83e2422
Alexey Viklov <alexey.viklov@gmail.com>
2023-12-14 22:23:57 +0300

    Finish line script updated

--- game/scripts/level/finish_line.gd
+++ game/scripts/level/finish_line.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 
+signal dog_finished(dog: Dog)
+
+
 func _on_body_exited(body: Node2D):
 	if body is Dog:
-#		body.fsm.transition_to("DogStateIdle")
-		body.position = Vector2(241, 413)
+		dog_finished.emit(body)
