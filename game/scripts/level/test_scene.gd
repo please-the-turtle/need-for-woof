b894a75faa36a476fff44cdf579987d855803159
Alexey Viklov <alexey.viklov@gmail.com>
2023-12-16 19:25:29 +0300

    Test scene changed

--- game/scripts/level/test_scene.gd
+++ game/scripts/level/test_scene.gd
@@ -12,3 +12,11 @@ func _ready():
 func _put_dog_on_start(dog: Dog):
 	#dog.position = Vector2(241, 413)
 	dog.fsm.transition_to("DogStateIdle")
+
+
+func _physics_process(_delta):
+	if Input.is_key_pressed(KEY_R):
+		$Siba.position = Vector2(241, 413)
+		$Siba.fsm.transition_to("DogStateRun")
+		$Siba2.position = Vector2(381, -314)
+		$Siba2.fsm.transition_to("DogStateIdle")
